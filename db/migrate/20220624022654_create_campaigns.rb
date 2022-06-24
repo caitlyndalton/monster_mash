9d7667f56bb443297189bc851e55a878a0db62ea
Caitlyn Dalton <cdalton0610@gmail.com>
2022-06-23 22:29:00 -0400

    Added controllers from schema

---
+++ db/migrate/20220624022654_create_campaigns.rb
@@ -0,0 +1,11 @@
+class CreateCampaigns < ActiveRecord::Migration[7.0]
+  def change
+    create_table :campaigns do |t|
+      t.integer :dm_id
+      t.string :name
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
