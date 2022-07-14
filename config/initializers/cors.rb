a478d6ed2aef73b28ae8fddba4529ca5c235c405
Caitlyn Dalton <cdalton0610@gmail.com>
2022-07-13 20:41:28 -0400

    added netlify to cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:8080", "https://dnd5eapi.co/api"
+    origins "localhost:8080", "https://dnd5eapi.co/api", "https://monster-mash.netlify.app/"
 
     resource "*",
       headers: :any,
